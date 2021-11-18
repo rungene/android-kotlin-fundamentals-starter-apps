9e2c4db12e5226c6bd0bfcb7a50c959bfe9b1652
rungene <rungenesamuel@gmail.com>
2021-11-18 05:15:35 +0300

    refactor:This commit adds onCleared() inside GameViewModel

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -10,4 +10,10 @@ class GameViewModel : ViewModel() {
         Log.i("GameViewModel","GameViewModel created")
     }
 
+
+    override fun onCleared() {
+        super.onCleared()
+
+        Log.i("GameViewModel","GameViewModel is destroyed ")
+    }
 }
