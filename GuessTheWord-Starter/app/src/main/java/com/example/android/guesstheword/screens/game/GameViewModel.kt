d1555fb804e9a6ff9beff27a3638101340ba90d9
rungene <rungenesamuel@gmail.com>
2021-11-18 05:12:32 +0300

    refactor:This commit adds init block

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,8 +1,13 @@
 package com.example.android.guesstheword.screens.game
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
+    //to illustrate Viewmodel is lifecycle aware
+    init {
+        Log.i("GameViewModel","GameViewModel created")
+    }
 
 }
