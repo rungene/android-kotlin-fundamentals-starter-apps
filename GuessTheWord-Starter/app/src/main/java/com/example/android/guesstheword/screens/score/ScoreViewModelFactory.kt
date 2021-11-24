50a32898c9c97f35cf002cb6c5e3c45656786689
rungene <rungenesamuel@gmail.com>
2021-11-24 05:22:47 +0300

    refactor:this commit implements unimplemented abstract method.

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
@@ -1,9 +1,15 @@
 package com.example.android.guesstheword.screens.score
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 //this class will be responsible for instantiating the ScoreViewModel object.
-class ScoreViewModelFactory(private val finalScore: Int):ViewModelProvider.Factory{
+class ScoreViewModelFactory(private val finalScore: Int):ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ScoreViewModel::class.java)) {
+            return ScoreViewModel(finalScore) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
 
-
+    }
 }
