8a4dc0e0212e8110575976f648540faf78e10a42
rungene <rungenesamuel@gmail.com>
2021-11-24 05:17:49 +0300

    refactor:This commit extend the ScoreViewModelFactory class from ViewModelProvider.Factory. Add a constructor parameter for the final score.

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
@@ -1,4 +1,9 @@
 package com.example.android.guesstheword.screens.score
 
-class ScoreViewModelFactory {
+import androidx.lifecycle.ViewModelProvider
+
+//this class will be responsible for instantiating the ScoreViewModel object.
+class ScoreViewModelFactory(private val finalScore: Int):ViewModelProvider.Factory{
+
+
 }
